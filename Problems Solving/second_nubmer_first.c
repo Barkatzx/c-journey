6d847a138bd5ea5491dc7c02e2790c1e9e8946cb
Barkatzx <barkatullah.zx@gmail.com>
2026-04-09 01:56:18 +0600

    Solve Problem

---
+++ Problems Solving/second_nubmer_first.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+    int a;
+    scanf("%d", &a);
+    printf("The first number is: %d\n", a);
+    int b;
+    scanf("%d", &b);
+    printf("The second number is: %d", b);
+    return 0;
+}
---
+++ Problems Solving/sum_two_number.c
@@ -0,0 +1,10 @@
+/*Take two integers A and B as input and output their summation.*/
+
+#include<stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A+B);
+    return 0;
+}
